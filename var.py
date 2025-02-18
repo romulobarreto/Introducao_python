bfe6293112e203532e90171dfda3f23496df26bb
Rômulo Barreto da Silva <romulo@MacBook-Air-de-Romulo.local>
2025-02-17 23:41:43 -0300

    docs: Variable and Data Explanation

---
+++ var.py
@@ -0,0 +1,15 @@
+# Variáveis e tipos de dados:
+
+# INPUT: Receber dados
+# STORAGE: Armazenar os dados
+# PROCESS: Processar os dados
+# OUTPUT: Exportar os dados
+
+# int (inteiro) Ex: 10
+# float (decimal) Ex: 10.1
+# str (string) Ex: 'Romulo Barreto'
+# bool (boleano) Ex: True or False
+
+idade = 21
+print(idade)
+
