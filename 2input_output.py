f6665cd0e6a0543b1a8ae0d3830b2628a3c6dbc9
ROMULO BARRETO DA SILVA <romubah@gmail.com>
2025-02-20 13:02:12 -0300

    feat: add example of user input and output in Python

---
+++ 2input_output.py
@@ -0,0 +1,11 @@
+# Entrada e saída de dados
+
+'''idade = 25
+nome = 'Rômulo Barreto'
+resultado = f'Olá {nome}, sua idade é: {idade} anos'
+
+print(resultado)'''
+
+idade = input('Digite a sua idade: \n')
+
+print(f'A sua idade é {25} anos.')
