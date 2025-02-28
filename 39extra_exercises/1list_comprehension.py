f3a4145ac19954839c50ae255fc04e0dcf151ec4
ROMULO BARRETO DA SILVA <romubah@gmail.com>
2025-02-28 13:47:07 -0300

    feat: add exercise of list comprehension

---
+++ 39extra_exercises/1list_comprehension.py
@@ -0,0 +1,26 @@
+'''
+1. Dobrar os números pares:
+Receba uma lista de números inteiros e crie uma nova lista contendo o dobro de cada número par da lista original.
+
+Exemplo de entrada: [1, 2, 3, 4, 5, 6]
+Exemplo de saída: [4, 8, 12]
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+2. Dobrar os números pares:
+Receba uma lista de números inteiros e crie uma nova lista contendo o dobro de cada número par da lista original.
+
+Exemplo de entrada: [1, 2, 3, 4, 5, 6]
+Exemplo de saída: [4, 8, 12]
+'''
